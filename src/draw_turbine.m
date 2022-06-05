30d4f818b0fc75cba17cad8f75f7d5da53b26bf3
Fewjetive <jacklove0824@gmail.com>
2022-06-05 17:07:22 +0800

    unused files

--- src/draw_turbine.m
+++
@@ -1,9 +0,0 @@
-n6049_path = "data\NACA 6409\n6409.dat";
-
-file = fopen(n6049_path, 'r');
-
-format = '%f %f';
-sizeA = [inf inf];
-A = fscanf(file, format, sizeA);
-
-fclose(file);
--- src/test.m
+++
@@ -1,5 +0,0 @@
-% t = 0:pi/50:10*pi;
-% st = sin(t);
-% ct = cos(t);
-% plot3(st,ct,t)
-[U_1, R, Omega, J, alpha, C_L, C_D] = read_data("NACA6409");
